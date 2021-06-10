822c8c7c915313588239f7a0fe80d164fc51819f
nameczz <zizhao.chen@zilliz.com>
2021-06-10 18:58:15 +0800

    add api

--- server/src/collections/collections.controller.ts
+++ server/src/collections/collections.controller.ts
@@ -21,6 +21,11 @@ export class CollectionsController {
     return await this.collectionsService.showCollections();
   }
 
+  @Get('statistics')
+  async getCollectionsIndexStatus() {
+    return await this.collectionsService.getCollectionsIndexStatus();
+  }
+
   @Post()
   @UsePipes(new ValidationPipe())
   async createCollection(@Body() data: CreateCollection) {
