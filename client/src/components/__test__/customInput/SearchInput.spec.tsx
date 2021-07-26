f93baef9e93f5f3e0c8703acd8f0dac3da2785b9
tumao <yan.wang@zilliz.com>
2021-07-26 15:57:39 +0800

    update search input test

--- client/src/components/__test__/customInput/SearchInput.spec.tsx
+++ client/src/components/__test__/customInput/SearchInput.spec.tsx
@@ -8,8 +8,28 @@ describe('test search input component', () => {
     const mockSearchFn = jest.fn();
     const container = render(
       <I18nextProvider i18n={i18n}>
+        <SearchInput searchText="search text" onSearch={mockSearchFn} />
+      </I18nextProvider>
+    );
+
+    // material textfield input role is textbox
+    expect(container.getByRole('textbox')).toBeInTheDocument();
+    expect(container.getByRole('textbox')).toHaveValue('search text');
+  });
+
+  it('checks input value change event', () => {
+    const mockSearchFn = jest.fn();
+    const container = render(
+      <I18nextProvider i18n={i18n}>
         <SearchInput onSearch={mockSearchFn} />
       </I18nextProvider>
     );
+
+    const input = container.getByRole('textbox');
+    fireEvent.change(input, { target: { value: 'search change test' } });
+    expect(input).toHaveValue('search change test');
+    // mock Enter key press event
+    fireEvent.keyPress(input, { key: 'Enter', code: 13, charCode: 13 });
+    expect(mockSearchFn).toBeCalledTimes(1);
   });
 });
