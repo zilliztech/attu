dcefda6014723f9ae5b92eaecb1ac804cfc67cc0
sutcalag <zhuanghong.chen@zilliz.com>
2021-07-28 19:07:37 +0800

    enable server side test case

--- server/src/collections/collections.service.spec.ts
+++ server/src/collections/collections.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsService } from './collections.service';
+import { MilvusServiceProvider } from '../milvus/milvus.service.mock';
 
 describe('CollectionsService', () => {
   let service: CollectionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CollectionsService],
+      providers: [CollectionsService, MilvusServiceProvider],
     }).compile();
 
     service = module.get<CollectionsService>(CollectionsService);
--- server/src/partitions/partitions.service.spec.ts
+++ server/src/partitions/partitions.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PartitionsService } from './partitions.service';
+import { MilvusServiceProvider } from '../milvus/milvus.service.mock';
 
 describe('PartitionsService', () => {
   let service: PartitionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PartitionsService],
+      providers: [PartitionsService, MilvusServiceProvider],
     }).compile();
 
     service = module.get<PartitionsService>(PartitionsService);
