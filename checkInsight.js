9218b44af3f956fedc42937c7b4bd06d9acf97b8
nameczz <zizhao.chen@zilliz.com>
2021-07-29 14:58:21 +0800

    test acion

---
+++ checkInsight.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const BASE_URL = process.env.INSIGHT_URL;
+console.log('---- check start ----- ', BASE_URL);
+
+axios
+  .get(`${BASE_URL}/api/v1/healthy`)
+  .then(res => {
+    console.log(res.data);
+    if (res.data.statusCode === 200) {
+      console.log('---- Server OK -----');
+    } else {
+      throw new Error('');
+    }
+  })
+  .then(err => {
+    throw new Error(err.message || '---- Server has some error ----');
+  });
+
+axios
+  .get(`${BASE_URL}/connect`)
+  .then(res => {
+    // if return statusCode mean it's failed. Otherwise it will return html
+    if (res.data.includes('<html')) {
+      console.log('---- Client OK -----');
+    } else {
+      throw new Error('---- Client has some error ----');
+    }
+  })
+  .catch(err => {
+    throw new Error(err.message || '---- Client has some error ----');
+  });
