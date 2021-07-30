fe8a7ec6eb461ca7eb10e586c0749a54890db931
nameczz <zizhao.chen@zilliz.com>
2021-07-30 18:41:04 +0800

    test error

--- checkInsight.js
+++ checkInsight.js
@@ -3,32 +3,19 @@ const axios = require('axios');
 const BASE_URL = process.env.INSIGHT_URL;
 console.log('---- check start ----- ', BASE_URL);
 
-axios
-  .get(`${BASE_URL}/api/v1/asd`)
-  .then(res => {
-    console.log(res.data);
-    if (res.data.statusCode === 200) {
-      console.log('---- Server OK -----');
-    } else {
-      throw new Error('');
-    }
-  })
-  .then(err => {
-    console.log('---- Server has some error ----');
-    process.exit();
-  });
+axios.get(`${BASE_URL}/api/v1/asd`).then(res => {
+  if (res.data.statusCode === 200) {
+    console.log('---- Server OK -----');
+  } else {
+    throw new Error('---- Server has some error ----');
+  }
+});
 
-axios
-  .get(`${BASE_URL}/connect`)
-  .then(res => {
-    // if return statusCode mean it's failed. Otherwise it will return html
-    if (res.data.includes('<html')) {
-      console.log('---- Client OK -----');
-    } else {
-      throw new Error('---- Client has some error ----');
-    }
-  })
-  .catch(err => {
-    console.log('---- Client has some error ----');
-    process.exit();
-  });
+axios.get(`${BASE_URL}/connect`).then(res => {
+  // if return statusCode mean it's failed. Otherwise it will return html
+  if (res.data.includes('<html')) {
+    console.log('---- Client OK -----');
+  } else {
+    throw new Error('---- Client has some error ----');
+  }
+});
