cb7b4b0c5b5dea45c8b0e9eaf0f96767065d2941
nameczz <zizhao.chen@zilliz.com>
2021-08-02 10:27:00 +0800

    add insight check action jobs

--- checkInsight.js
+++ checkInsight.js
@@ -2,11 +2,10 @@ const axios = require('axios');
 const core = require('@actions/core');
 
 const BASE_URL = process.env.INSIGHT_URL;
-console.log('---- check start ----- ', BASE_URL);
 
 const check = async () => {
   const clientRes = await axios.get(`${BASE_URL}/connect`);
-  const serverRes = await axios.get(`${BASE_URL}/api/v1/asd`);
+  const serverRes = await axios.get(`${BASE_URL}/api/v1/healthy`);
   if (serverRes.data.statusCode === 200) {
     console.log('---- Server OK -----');
   } else {
