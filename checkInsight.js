93fdd5a1cc78ec74241ddd7757efb908779f4091
nameczz <zizhao.chen@zilliz.com>
2021-08-02 09:58:22 +0800

    test error

--- checkInsight.js
+++ checkInsight.js
@@ -3,19 +3,20 @@ const axios = require('axios');
 const BASE_URL = process.env.INSIGHT_URL;
 console.log('---- check start ----- ', BASE_URL);
 
-axios.get(`${BASE_URL}/api/v1/asd`).then(res => {
-  if (res.data.statusCode === 200) {
+const check = async () => {
+  const clientRes = await axios.get(`${BASE_URL}/connect`);
+  const serverRes = await axios.get(`${BASE_URL}/api/v1/asd`);
+  if (serverRes.data.statusCode === 200) {
     console.log('---- Server OK -----');
   } else {
     throw new Error('---- Server has some error ----');
   }
-});
 
-axios.get(`${BASE_URL}/connect`).then(res => {
-  // if return statusCode mean it's failed. Otherwise it will return html
-  if (res.data.includes('<html')) {
+  if (clientRes.data.includes('<html')) {
     console.log('---- Client OK -----');
   } else {
     throw new Error('---- Client has some error ----');
   }
-});
+};
+
+check();
