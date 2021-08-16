da66815319501faf12aeacbe136c503584e77ecc
sutcalag <zhuanghong.chen@zilliz.com>
2021-08-16 15:18:42 +0800

    add community btn

--- client/src/i18n/en/common.ts
+++ client/src/i18n/en/common.ts
@@ -23,6 +23,15 @@ const commonTrans = {
   },
   param: 'Parameter',
   search: 'Search by name',
+  community: {
+    hi: 'Hi, there!',
+    growing: 'Our growing community is here!',
+    question: 'Have question about Milvus?',
+    qr: 'Scan WeChat QR code to get access',
+    more: 'More Channels to Explore',
+    join: 'Join our growing social community today',
+    get: 'Get insight, tips and share ideas'
+  },
 };
 
 export default commonTrans;
