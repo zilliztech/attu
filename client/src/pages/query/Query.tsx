3a0f18d8d70e3a356ff84c36782ef3123b0aa075
czhen <zhen.chen@zilliz.com>
2021-11-09 17:13:37 +0800

    refine code

--- client/src/pages/query/Query.tsx
+++ client/src/pages/query/Query.tsx
@@ -13,8 +13,6 @@ import icons from '../../components/icons/Icons';
 import CustomButton from '../../components/customButton/CustomButton';
 import MilvusGrid from '../../components/grid/Grid';
 import { getQueryStyles } from './Styles';
-import CustomToolBar from '../../components/grid/ToolBar';
-import { ColDefinitionsType, ToolBarConfig } from '../../components/grid/Types';
 import Filter from '../../components/advancedSearch';
 import { CollectionHttp } from '../../http/Collection';
 import { FieldHttp } from '../../http/Field';
@@ -34,6 +32,7 @@ const Query: FC<{
   const { t: searchTrans } = useTranslation('search');
   const { t: collectionTrans } = useTranslation('collection');
   const { t: btnTrans } = useTranslation('btn');
+
   const classes = getQueryStyles();
 
   const {
@@ -47,6 +46,7 @@ const Query: FC<{
     orderBy,
     handleGridSort,
   } = usePaginationHook(queryResult || []);
+
   const handlePageChange = (e: any, page: number) => {
     handleCurrentPage(page);
   };
@@ -57,23 +57,23 @@ const Query: FC<{
       name: i.name,
       type: i.data_type.includes('Int') ? 'int' : 'float',
     }));
-    console.log(schemaList);
     setFields(nameList);
   };
 
+  // Get fields at first or collection name changed.
   useEffect(() => {
     collectionName && getFields(collectionName);
   }, [collectionName]);
 
   const filterRef = useRef();
-  const resetFilter = () => {
+
+  const handleFilterReset = () => {
     const currentFilter: any = filterRef.current;
     currentFilter?.getReset();
     setExpression('');
     setTableLoading(null);
     setQueryResult(null);
   };
-
   const handleFilterSubmit = (expression: string) => {
     setExpression(expression);
   };
@@ -85,9 +85,7 @@ const Query: FC<{
         output_fields: fields.map(i => i.name),
       });
       setTableLoading(false);
-      console.log(res);
       const result = res.data;
-      // const result = transferSearchResult(res.results);
       setQueryResult(result);
     } catch (err) {
       setTableLoading(false);
@@ -112,7 +110,7 @@ const Query: FC<{
           />
         </div>
         <div className="right">
-          <CustomButton className="btn" onClick={resetFilter}>
+          <CustomButton className="btn" onClick={handleFilterReset}>
             <ResetIcon classes={{ root: 'icon' }} />
             {btnTrans('reset')}
           </CustomButton>
@@ -134,28 +132,15 @@ const Query: FC<{
             disablePadding: false,
             label: i.name,
           }))}
-          // rows={queryResult || []}
-          // rowCount={queryResult?.length || 0}
           primaryKey={fields.find(i => i.is_primary_key)?.name}
-          // page={1}
-          // onChangePage={() => {}}
-          // rowsPerPage={1000}
-          // setRowsPerPage={() => {}}
           openCheckBox={false}
-          // isLoading={tableLoading}
           isLoading={!!tableLoading}
-          // orderBy={fields.find(i => i.is_primary_key)?.name}
-          // order={''}
-          // handleSort={() => {}}
           rows={result}
           rowCount={total}
-          // primaryKey="rank"
           page={currentPage}
           onChangePage={handlePageChange}
           rowsPerPage={pageSize}
           setRowsPerPage={handlePageSize}
-          // openCheckBox={false}
-          // isLoading={tableLoading}
           orderBy={orderBy}
           order={order}
           handleSort={handleGridSort}
