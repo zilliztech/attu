88a49cdf9bec23fad421d3ba8d455852759dc494
Gitea <zizhao.chen@zilliz.com>
2021-11-10 17:21:27 +0800

    refine milvus controller;
    Signed-off-by: Gitea <zizhao.chen@zilliz.com>

---
+++ express/src/milvus/dto.ts
@@ -0,0 +1,17 @@
+import { ArrayMinSize, IsArray, IsString } from "class-validator";
+
+export class ConnectMilvusDto {
+  @IsString()
+  readonly address: string;
+}
+
+export class CheckMilvusDto {
+  @IsString()
+  readonly address: string;
+}
+
+export class FlushDto {
+  @IsArray()
+  @ArrayMinSize(1, { message: "At least need one collection name." })
+  readonly collection_names: string[];
+}
