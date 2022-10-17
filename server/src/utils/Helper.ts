ae5ba2801b9d870af6c764102dd4592018dca3a0
shanghaikid <jiangruiyi@gmail.com>
2022-10-17 18:02:35 +0800

    fix build

--- server/src/utils/Helper.ts
+++ server/src/utils/Helper.ts
@@ -45,9 +45,9 @@ export const genRows = (fields: FieldSchema[], size: number) => {
 
 export const makeRandomId = (length: number): string => {
   var result = '';
-  var characters =
+  const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
+  const charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
