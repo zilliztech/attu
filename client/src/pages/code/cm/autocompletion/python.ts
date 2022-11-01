a6a0ae6226dee4828363f150bf63c297e717c825
shanghaikid <jiangruiyi@gmail.com>
2022-11-01 17:05:50 +0800

    completion part2

--- client/src/pages/code/cm/autocompletion/python.ts
+++ client/src/pages/code/cm/autocompletion/python.ts
@@ -4,65 +4,22 @@ import {
 } from '@codemirror/autocomplete';
 export const keywords = [
   { label: 'False', type: 'keyword' },
-  { label: 'await', type: 'keyword' },
-  { label: 'else', type: 'keyword' },
   { label: 'import', type: 'keyword' },
-  { label: 'pass', type: 'keyword' },
-  { label: 'None', type: 'keyword' },
-  { label: 'break', type: 'keyword' },
-  { label: 'except', type: 'keyword' },
   { label: 'in', type: 'keyword' },
-  { label: 'raise', type: 'keyword' },
   { label: 'True', type: 'keyword' },
-  { label: 'class', type: 'keyword' },
-  { label: 'finally', type: 'keyword' },
   { label: 'is', type: 'keyword' },
-  { label: 'return', type: 'keyword' },
-  { label: 'and', type: 'keyword' },
-  { label: 'continue', type: 'keyword' },
-  { label: 'for', type: 'keyword' },
-  { label: 'lambda', type: 'keyword' },
-  { label: 'try', type: 'keyword' },
-  { label: 'as', type: 'keyword' },
-  { label: 'def', type: 'keyword' },
   { label: 'from', type: 'keyword' },
-  { label: 'nonlocal', type: 'keyword' },
-  { label: 'while', type: 'keyword' },
-  { label: 'asset', type: 'keyword' },
-  { label: 'del', type: 'keyword' },
-  { label: 'global', type: 'keyword' },
   { label: 'not', type: 'keyword' },
-  { label: 'with', type: 'keyword' },
-  { label: 'async', type: 'keyword' },
-  { label: 'elif', type: 'keyword' },
-  { label: 'if', type: 'keyword' },
   { label: 'or', type: 'keyword' },
-  { label: 'yield', type: 'keyword' },
 ];
 
 export const pymilvusKeywords = [
-  { label: 'CollectionSchema', type: 'variable' },
-  { label: 'DataType', type: 'variable' },
-  { label: 'Collection', type: 'variable' },
-];
-
-export const pymilvusObjectKeyMap: any = {
-  Collection: [
-    { label: 'create_index()', type: 'variable' },
-    { label: 'create_parititon()', type: 'variable' },
-    { label: 'delete()', type: 'variable' },
-    { label: 'drop_index()', type: 'variable' },
-    { label: 'drop_partition()', type: 'variable' },
-    { label: 'index()', type: 'variable' },
-    { label: 'insert()', type: 'variable' },
-    { label: 'load()', type: 'variable' },
-    { label: 'partition()', type: 'variable' },
-    { label: 'query()', type: 'variable' },
-    { label: 'release()', type: 'variable' },
-    { label: 'search()', type: 'variable' },
-    { label: 'compact()', type: 'variable' },
-  ],
-};
+  'CollectionSchema',
+  'DataType',
+  'Collection',
+  'Partition',
+  'FieldSchema',
+].map(k => ({ label: k, type: 'variable' }));
 
 export const variables = [
   { label: 'schema', type: 'variable' },
--- client/src/pages/code/cm/completion.ts
+++ client/src/pages/code/cm/completion.ts
@@ -4,57 +4,25 @@ import {
   keywords,
   pymilvusKeywords,
   variables,
-  pymilvusObjectKeyMap,
   snippets,
 } from './autocompletion/python';
 
-const objectKeys = Object.keys(pymilvusObjectKeyMap);
-
 export function autocomplete(context: CompletionContext) {
-  const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1);
+  const current = syntaxTree(context.state).resolveInner(context.pos, -1);
   const word = context.matchBefore(/\w*/);
   const token = context.tokenBefore(['VariableName']);
 
-  console.log(nodeBefore, 'word', word, token);
+  console.log(current, 'word', word, token);
 
   if (
-    nodeBefore &&
-    nodeBefore.prevSibling &&
-    nodeBefore.prevSibling.name === 'from'
+    current &&
+    current.prevSibling &&
+    current.prevSibling.name === 'from'
   ) {
     return {
-      from: nodeBefore.prevSibling.to + 1,
+      from: current.prevSibling.to + 1,
       options: [{ label: 'pymilvus', type: 'milvus' }],
     };
-  }
-
-  if (
-    nodeBefore &&
-    nodeBefore.name === 'VariableName' &&
-    word &&
-    word.from > 0
-  ) {
-    return {
-      from: word!.from,
-      options: [...variables, ...pymilvusKeywords, ...snippets],
-    };
-  }
-
-  if (nodeBefore.name === '.' || nodeBefore.name === 'PropertyName') {
-    const previousNode = nodeBefore!.prevSibling;
-    const textBefore = context.state.sliceDoc(
-      previousNode!.from,
-      context.pos - 1
-    );
-
-    console.log(nodeBefore, previousNode, 'textBefore', textBefore);
-
-    if (objectKeys.indexOf(textBefore) != -1) {
-      return {
-        from: word!.from,
-        options: [...pymilvusObjectKeyMap[textBefore]],
-      };
-    }
   }
 
   if (word!.from == word!.to && !context.explicit) return null;
