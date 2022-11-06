9df0cc2c537a1245e960ea1ba18b7230594b6245
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-11-06 21:24:08 +0800

    fix CustomButton test

--- client/src/components/__test__/customButton/CustomButton.spec.tsx
+++ client/src/components/__test__/customButton/CustomButton.spec.tsx
@@ -1,40 +1,11 @@
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
 import CustomButton from '../../customButton/CustomButton';
-import { vi } from 'vitest';
-
-let container: any = null;
-
-vi.mock('@material-ui/core/Button', () => {
-  return (props: any) => {
-    const { variant, children } = props;
-    return (
-      <>
-        <div className="variant">{variant}</div>
-        <button className="button">{children}</button>;
-      </>
-    );
-  };
-});
+import { render } from '@testing-library/react';
 
 describe('Test CustomButton', () => {
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-  });
-
   test('test button props', () => {
-    act(() => {
-      render(<CustomButton variant="contained">test</CustomButton>, container);
-    });
-
-    expect(container.querySelector('.button').textContent).toBe('test');
-    expect(container.querySelector('.variant').textContent).toBe('contained');
+    const result = render(
+      <CustomButton variant="contained">test</CustomButton>
+    );
+    expect(result.getByText('test').textContent).toBe('test');
   });
 });
--- client/src/components/__test__/customDialog/CustomDialogTitle.spec.tsx
+++ client/src/components/__test__/customDialog/CustomDialogTitle.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import CustomDialogTitle from '../../customDialog/CustomDialogTitle';
 import { vi } from 'vitest';
 
