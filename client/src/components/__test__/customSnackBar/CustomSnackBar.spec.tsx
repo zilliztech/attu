93f31ca6a6ca5739304451bb234913e669ad97f5
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-11-06 20:57:10 +0800

    fx customSnackBar test

--- client/src/components/__test__/customSnackBar/CustomSnackBar.spec.tsx
+++ client/src/components/__test__/customSnackBar/CustomSnackBar.spec.tsx
@@ -1,32 +1,12 @@
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
 import { SnackBarType } from '../../../context/Types';
 import CustomSnackBar from '../../customSnackBar/CustomSnackBar';
 import { vi } from 'vitest';
 
-let container: any = null;
-
-vi.mock('@material-ui/core/Snackbar', () => {
-  return (props: any) => {
-    return <div id="snackbar">{props.children}</div>;
-  };
-});
-
 describe('Test Custom Dialog', () => {
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-  });
-
   it('Test Custom dialog ', () => {
     const params: SnackBarType = {
-      open: false,
+      open: true,
       type: 'success',
       message: 'test',
       vertical: 'top',
@@ -34,12 +14,7 @@ describe('Test Custom Dialog', () => {
       autoHideDuration: 2000,
     };
     const handleClose = vi.fn();
-    act(() => {
-      render(<CustomSnackBar {...params} onClose={handleClose} />, container);
-    });
-
-    expect(container.querySelector('#snackbar').textContent).toEqual(
-      params.message
-    );
+    render(<CustomSnackBar {...params} onClose={handleClose} />);
+    expect(screen.queryByText('test')?.textContent).toEqual(params.message);
   });
 });
