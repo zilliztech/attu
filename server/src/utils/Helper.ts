2f793a0f2a2e037b57f5a8cc52d0ac13f74eb469
shanghaikid <jiangruiyi@gmail.com>
2022-11-07 09:36:46 +0800

    add missing dependency

--- server/src/utils/Helper.ts
+++ server/src/utils/Helper.ts
@@ -44,11 +44,11 @@ export const genRows = (fields: FieldSchema[], size: number) => {
 };
 
 export const makeRandomId = (length: number): string => {
-  var result = '';
+  let result = '';
   const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
