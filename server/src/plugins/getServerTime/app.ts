d3e16bea894c984fbb5b489fb6ef69d6b378a115
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-12-22 10:31:51 +0800

    remove unused files

--- server/src/plugins/getServerTime/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  return res.json({ timestamp: new Date().getTime()});
-});
-
-export default { router };
