09f572fc77608e708195d6aea516b077c6c228b2
ryjiang <jiangruiyi@gmail.com>
2024-01-11 14:28:11 +0800

    fix i18n
    Signed-off-by: ryjiang <jiangruiyi@gmail.com>

--- client/src/i18n/cn/success.ts
+++ client/src/i18n/cn/success.ts
@@ -1,11 +1,12 @@
 const successTrans = {
   connect: '成功连接到milvus',
-  create: `{{name}}已被创建。`,
+  create: `{{name}}创建成功。`,
   load: `{{name}}正在加载。`,
-  delete: `{{name}}已成功删除。`,
-  release: `{{name}}已被释放。`,
-  update: `{{name}}已被更新。`,
-  rename: `{{name}}已被重命名。`,
+  delete: `{{name}}删除成功。`,
+  release: `{{name}}释放成功。`,
+  update: `{{name}}更新成功。`,
+  rename: `{{name}}重命名成功。`,
+  duplicate: `{{name}}复制成功.`,
 };
 
 export default successTrans;
