cb933193121c082ceade769cdfd52e406101ce31
Shuyou <shuyou.zhan@zilliz.com>
2024-04-09 10:54:18 +0800

    pref: build docker image locally

---
+++ scripts/build-release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# login docker hub
+source scripts/login.sh
+
+# prepare environment
+source scripts/prepare.sh
+
+command docker buildx build \
+  --platform linux/arm64,linux/amd64 \
+  --tag zilliz/attu:${TAG_NAME},zilliz/attu:latest \
+  --build-arg VERSION=${TAG_NAME} \
+  --file Dockerfile --push .
+
+command docker buildx rm multiarch
