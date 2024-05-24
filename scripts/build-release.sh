7ae5ad17d06f128c96b2355dbec3529e389a7a99
ryjiang <jiangruiyi@gmail.com>
2024-05-24 16:05:24 +0800

    Created additional semver container tags
    Signed-off-by: ryjiang <jiangruiyi@gmail.com>

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -6,11 +6,14 @@ source scripts/login.sh
 # prepare environment
 source scripts/prepare.sh
 
+# Build and push Docker images with both tags
 command docker buildx build \
   --platform linux/arm64,linux/amd64 \
   --tag zilliz/attu:${TAG_NAME} \
+  --tag zilliz/attu:${MAJOR_MINOR} \
   --tag zilliz/attu:latest \
   --build-arg VERSION=${TAG_NAME} \
   --file Dockerfile --push .
 
+# Remove buildx instance
 command docker buildx rm multiarch
