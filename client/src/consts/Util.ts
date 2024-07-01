e4e779fb5ba01c6c2f2beac8d4f3cb221226fe7b
Rohit Gupta <r.gupta2508@gmail.com>
2024-07-01 14:01:45 +0530

    Update Util.ts

--- client/src/consts/Util.ts
+++ client/src/consts/Util.ts
@@ -54,6 +54,10 @@ export const LOGICAL_OPERATORS = [
     value: 'ARRAY_CONTAINS_ALL',
     label: 'ARRAY_CONTAINS_ALL',
   },
+  {
+    value: 'ARRAY_CONTAINS_ANY',
+    label: 'ARRAY_CONTAINS_ANY',
+  },
 ];
 
 export enum FILE_MIME_TYPE {
