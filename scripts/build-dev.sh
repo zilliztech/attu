3e6b79fd1e7e3fa74e58e7d02e3a99d1b39aea67
shanghaikid <jiangruiyi@gmail.com>
2024-09-07 16:42:10 +0800

    feat: support arm v7
    Signed-off-by: shanghaikid <jiangruiyi@gmail.com>

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -7,7 +7,7 @@ source scripts/login.sh
 source scripts/prepare.sh
 
 command docker buildx build \
-  --platform linux/arm64,linux/amd64 \
+  --platform linux/arm64,linux/arm/v7,linux/amd64 \
   --tag zilliz/attu:dev \
   --build-arg VERSION=dev \
   --file Dockerfile --push .
