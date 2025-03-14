7496ad4793a8d63f8153e3fa506f2d2c49ea4b57
ryjiang <jiangruiyi@gmail.com>
2025-03-14 14:12:55 +0800

    chore: remove unused code
    Signed-off-by: ryjiang <jiangruiyi@gmail.com>

--- server/src/prometheus/index.ts
+++
@@ -1,6 +0,0 @@
-import { PrometheusController } from './prometheus.controller';
-
-const prometheusManager = new PrometheusController();
-const router = prometheusManager.generateRoutes();
-
-export { router };
