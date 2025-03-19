691d73ae8e5145b211377c950dc9052d671e9915
ryjiang <jiangruiyi@gmail.com>
2025-03-19 14:52:50 +0800

    chore: update packages (#807)
    Signed-off-by: ryjiang <jiangruiyi@gmail.com>

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -84,44 +84,29 @@ export const resources = {
 
 // the translations
 // (tip move them in a JSON file and import them)
-
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     resources,
-    keySeparator: false, // we do not use keys in form messages.welcome
+    keySeparator: false,
     returnObjects: true,
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
-
     detection: {
-      // order and from where user language should be detected
       order: ['localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
-
-      // keys or params to lookup language from
       lookupLocalStorage: 'attu.ui.lang',
       lookupFromPathIndex: 0,
       lookupFromSubdomainIndex: 0,
-
-      // cache user language on
       caches: ['localStorage', 'cookie'],
-      excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
-
-      // optional expire and domain for set cookie
+      excludeCacheFor: ['cimode'],
       cookieMinutes: 10,
-      cookieDomain: 'myDomain',
-
-      // optional htmlTag with lang attribute, the default is:
+      cookieDomain: '',
       htmlTag: document.documentElement,
-
-      // only detect languages that are in the whitelist
-      checkWhitelist: true,
     },
+    supportedLngs: ['en', 'zh-CN'], // Add your supported languages here
   });
 
 export default i18n;
